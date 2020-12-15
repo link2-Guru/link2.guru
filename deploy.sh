05054612ec57734a7cc8c2712c42f1654a0e748e
wei <503945930@qq.com>
2020-12-15 23:43:05 +0800

    ff

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,11 @@ rm -rf public
 mkdir public
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
-
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+hugo --publishDir = docs
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
-
-echo "Push to origin"
-git push origin gh-pages
+echo "Push to origin master"
+git push origin master
